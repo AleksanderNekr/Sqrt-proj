f6ad43692af9e8e1cd721fb172979d92a5192add
Александр Некрасов <aleksandr_nekrasov@outlook.com>
2021-09-02 22:15:38 +0500

    Добавьте файлы проекта.

---
+++ Sqrt/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Sqrt
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <param name="disposing"
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SQRT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(0, 185);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(584, 116);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SQRT
+            // 
+            this.SQRT.Font = new System.Drawing.Font("Segoe UI", 39F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SQRT.Location = new System.Drawing.Point(13, 13);
+            this.SQRT.Name = "SQRT";
+            this.SQRT.Size = new System.Drawing.Size(559, 166);
+            this.SQRT.TabIndex = 1;
+            this.SQRT.Text = "Вывести корень из числа";
+            this.SQRT.UseVisualStyleBackColor = true;
+            this.SQRT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 301);
+            this.Controls.Add(this.SQRT);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SQRT;
+    }
+}
+
---
+++ Sqrt/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sqrt
+{
+    public partial class Form1 : Form
+    {
+        public string Sqrt;
+        public string Num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Num = textBox1.Text;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sn1, res;
+            sn1 = Convert.ToDouble(Num);
+            res = Math.Sqrt(sn1);
+            textBox1.Text = res.ToString();
+        }
+
+    }
+}
