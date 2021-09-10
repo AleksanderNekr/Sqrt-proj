2709ec5399c0a39d07a9f6015535fdcb57cea767
Александр Некрасов <aleksandr_nekrasov@outlook.com>
2021-09-10 18:18:54 +0500

    Added comments

--- Sqrt/Form1.cs
+++ Sqrt/Form1.cs
@@ -11,10 +11,13 @@ namespace Sqrt
         public string NumberText;
         public SquareRootProgram()
         {
+            // Checking for the presence of a language group
             if (!string.IsNullOrEmpty(Properties.Settings.Default.Language))
             {
+                // Loading default language
                 System.Threading.Thread.CurrentThread.CurrentUICulture =
                     CultureInfo.GetCultureInfo(Properties.Settings.Default.Language);
+                // Applying the separator style - dots
                 System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
             }
             InitializeComponent();
@@ -22,6 +25,7 @@ namespace Sqrt
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Loading languages
             comboBox1.DataSource = new CultureInfo[]
             {
                 CultureInfo.GetCultureInfo("ru-RU"),
@@ -33,12 +37,14 @@ namespace Sqrt
             comboBox1.DisplayMember = "NativeName";
             comboBox1.ValueMember = "Name";
 
+            // Applying language
             if (!string.IsNullOrEmpty(Properties.Settings.Default.Language))
             {
                 comboBox1.SelectedValue = Properties.Settings.Default.Language;
             }
         }
 
+        // Saving language 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Properties.Settings.Default.Language = comboBox1.SelectedValue.ToString();
@@ -51,27 +57,40 @@ namespace Sqrt
             Process.Start(Application.StartupPath + "\\Sqrt.NET.exe");
         }
 
+        // Calculation of arithmetic and complex roots
         private void ComplexSqrt_Click(object sender, EventArgs e)
         {
+            // Checking for an empty input string
             double ConvertTextbox(string text) => text == "" ? 0.0 : double.Parse(text);
 
+            // Reading the real and complex parts of a number
             double NumRE = ConvertTextbox(textBoxRE.Text);
             double NumIM = ConvertTextbox(textBoxIM.Text);
+
+            // Creating a complex number
             Complex ComplexNum = new Complex(NumRE, NumIM);
 
+            // Square root function and rounding a number
             string GetSqrt(double number)
             {
                 return Math.Round(number, int.Parse(DecPointDigitsCount.Text))
                     .ToString($"F{DecPointDigitsCount.Text}");
             }
+
+            // Derivation of the real and complex parts of a number
             textBoxRE.Text = GetSqrt(Complex.Sqrt(ComplexNum).Real);
             textBoxIM.Text = GetSqrt(Complex.Sqrt(ComplexNum).Imaginary);
         }
 
+        // Clear text buttons
         private void ClearButtonRE_Click(object sender, EventArgs e) => textBoxRE.Text = "0";
 
         private void ClearButtonIM_Click(object sender, EventArgs e) => textBoxIM.Text = "0";
 
+        // Function that prevents "bad input":
+        // only numbers are accepted as input,
+        // it is possible to press the "Backspase" key,
+        // and check for the number of separators (only 1)
         public bool PreventBadText(string text, char key)
         {
             return !(char.IsDigit(key) || key == 8 || !text.Contains('.') && key == '.');
@@ -87,6 +106,7 @@ namespace Sqrt
             e.Handled = PreventBadText(textBoxIM.Text, e.KeyChar);
         }
 
+        // Function that reverses the sign of a number
         public string ChangeSign(string text)
         {
             return (-1 * double.Parse(text)).ToString($"F{DecPointDigitsCount.Text}");
@@ -102,6 +122,7 @@ namespace Sqrt
             textBoxIM.Text = ChangeSign(textBoxIM.Text);
         }
 
+        // Functions for copying numbers using buttons
         private void CopyBtnRE_Click(object sender, EventArgs e)
         {
             Clipboard.SetData(DataFormats.Text, textBoxRE.Text);
@@ -112,75 +133,34 @@ namespace Sqrt
             Clipboard.SetData(DataFormats.Text, textBoxIM.Text);
         }
 
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "x";
-        }
+        // Functions adding operators for analytic roots
+        private void Button2_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "x";
 
-        private void SinBtn_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "Sin(x)";
-        }
+        private void SinBtn_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "Sin(x)";
 
-        private void ClearBtnAnalityc_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text = "";
-        }
+        private void ClearBtnAnalityc_Click(object sender, EventArgs e) => TextBoxAnalityc.Text = "";
 
-        private void CosBtn_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "Cos(x)";
-        }
+        private void CosBtn_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "Cos(x)";
 
-        private void TanBtn_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "Tan(x)";
-        }
+        private void TanBtn_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "Tan(x)";
 
-        private void CotBtn_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "Cot(x)";
-        }
+        private void CotBtn_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "Cot(x)";
 
-        private void Pow2Btn_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "²";
-        }
+        private void Pow2Btn_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "²";
 
-        private void TextBoxAnalityc_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            e.Handled = true;
-        }
+        private void TextBoxAnalityc_KeyPress(object sender, KeyPressEventArgs e) => e.Handled = true;
 
-        private void BtnPlus_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "+";
-        }
+        private void BtnPlus_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "+";
 
-        private void BtnMinus_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "-";
-        }
+        private void BtnMinus_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "-";
 
-        private void BtnLeftBr_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "(";
-        }
+        private void BtnLeftBr_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "(";
 
-        private void BtnRightBr_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += ")";
-        }
+        private void BtnRightBr_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += ")";
 
-        private void BtnMult_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "×";
-        }
+        private void BtnMult_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "×";
 
-        private void BtnDiv_Click(object sender, EventArgs e)
-        {
-            TextBoxAnalityc.Text += "/";
-        }
+        private void BtnDiv_Click(object sender, EventArgs e) => TextBoxAnalityc.Text += "/";
 
         private void AnalyticSqrt_Click(object sender, EventArgs e)
         {
